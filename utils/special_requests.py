a443915662b9986ff48c157180ab4b856fc70f0a
Svetlana1234567891011 <153299652+Svetlana1234567891011@users.noreply.github.com>
2024-06-19 18:26:17 +0300

    Update special_requests.py

--- utils/special_requests.py
+++ utils/special_requests.py
@@ -18,39 +18,25 @@ class BaseRequests:
     def post_request_transform_token(self, url, data, token):
         headers = {'authorization': token}
         response = requests.post(url=url, data=data, headers=headers)
-        # if response.headers.get('Content-Type') and 'application/json' in response.headers['Content-Type']:
+        
         return response.json()
 
     def post_request_transform(self, url, data):
         response = requests.post(url=url, json=data)
         if response.headers.get('Content-Type') and 'application/json' in response.headers['Content-Type']:
-            # if 'application/json' in response.headers['Content-Type']:
-            #     return response.json()
-            # else:
-            #     return response.text
-            # return {"status_code": response.status_code, "text": response.json()}
+            
             return response.json()
         else:
             return {"status_code": response.status_code, "text": response.text}
 
-    # def exec_delete_request(self, url, token):
-    #     headers = {"Content-Type": "application/json", 'authorization': token}
-    #     response = requests.delete(url=url, headers=headers)
-    #     if 'application/json' in response.headers['Content-Type']:
-    #         return response.json()
-    #     else:
-    #         return response.text
+    
 
 
 class UserRequests(BaseRequests):
     user_handler = 'api/auth/register'
     manipulate_user_handler = 'api/auth/user'
 
-    # @allure.step('Создаем пользователя, отправив запрос POST')
-    # def post_create_user(self, data):
-    #     url = f"{self.host}{self.user_handler}"
-    #     response = requests.post(url, data=data)
-    #     return response
+    
     @allure.step('Создаем пользователя, отправив запрос POST')
     def post_create_courier(self, data=None):
         url = f"{self.host}{self.user_handler}"
